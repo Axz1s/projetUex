467117f3ab38c8015607407a6fa24d35368195bf
Bufaloo <killian-r@live.fr>
2018-12-19 16:44:04 +0100

    Mise a jour de l'index avec les adresses IP

--- index.php
+++ index.php
@@ -127,13 +127,72 @@ function TestMasqueSousReseau(IP1, IP2, IP3, IP4) {
         return 8;
     } return 0;
   }
+  function plageIP($ip, $masque){
+    $SR1 = 32 - $masque;
+    $nbrdeMachine = pow(2, $SR1) - 2;
+ 
+    //converti masque en bit
+    $SR2 = $masque;
+    $masquebinaire = "";
+    $i=0;
+    while ($i<32){
+        if($SR2<=0){
+            $masquebinaire.="0";
+        }else{
+            $masquebinaire.="1";
+            $SR2-=1;
+        }
+        $i++;
+    }
+ 
+    //$converti ip en bit
+    $i=0;
+    foreach ($ip as $element) {
+        $ip[$i] = decbin(intval($element));
+        while(strlen($ip[$i])<8){
+            $ip[$i] = intval("O").$ip[$i];
+        }
+        $i++;
+    }
+    
+    $adressIpBinaire = join( '' , $ip);
+    $adressReseau = decoupageIp($adressIpBinaire, $SR1, 0, 0);
+    $adressBroadcast = decoupageIp($adressIpBinaire, $SR1, 1, 1);
+    $adressePremiereMachine = decoupageIp($adressIpBinaire, $SR1, 0, 1);
+    $adresseDerniereMachine = decoupageIp($adressIpBinaire, $SR1, 1, 0);  
+ 
+    return $list = [join( '.' , $adressReseau),
+                    join( '.' , listeIp($masquebinaire)),
+                    join( '.' , $adressBroadcast),
+                    $nbrdeMachine,
+                    join( '.' , $adressePremiereMachine),
+                    join( '.' , $adresseDerniereMachine),];
+}
+ 
+function listeIp($IpBinaire){
+    $listeIp = array();
+    array_push($listeIp, bindec(substr($IpBinaire, 0, -24)));
+    array_push($listeIp, bindec(substr($IpBinaire, 8, -16)));
+    array_push($listeIp, bindec(substr($IpBinaire, 16, -8)));
+    array_push($listeIp, bindec(substr($IpBinaire, 24)));
+    return $listeIp;
+}
+ 
+function decoupageIp($adressIp, $SR1, $intval1, $intval2){
+    $return = substr($adressIp, 0, -$SR1);
+    while(strlen($return)<31){
+            $return .=intval("$intval1");
+    }
+    $return .=intval("$intval2");
+    return listeIp($return);
+}
       //-->
     </script>
 </head>
 <body>
 
     <div class="Titre">
-        <h1> Calculetron </h1>
+        <h1> Calculatron </h1>
 
     </div>
 
@@ -182,6 +241,7 @@ function TestMasqueSousReseau(IP1, IP2, IP3, IP4) {
                   <input type="text" name="SR3" size="3" maxlength="3" value="255" onClick="ClearBroadcast(this.form)">
                   
                   <input type="text" name="SR4" size="3" maxlength="3" value="0" onClick="ClearBroadcast(this.form)">
+                  
                   <input type="button" value="Calculer" name="B1" onClick="CalcBroadcast(this.form)"></p> 
                 </td>
               </tr>
@@ -209,7 +269,7 @@ function TestMasqueSousReseau(IP1, IP2, IP3, IP4) {
                   <div>Première adresse IP délivrée : </div>
                 </td>
                 <td> 
-                  <input type="text" name="Broadcast" size="20">
+                  <input type="text" name="Premiere" size="20">
                 </td>
               </tr>
               <tr> 
@@ -217,7 +277,7 @@ function TestMasqueSousReseau(IP1, IP2, IP3, IP4) {
                   <div>Dernière adresse IP délivrée : </div>
                 </td>
                 <td> 
-                  <input type="text" name="Broadcast" size="20">
+                  <input type="text" name="Derniere" size="20">
                 </td>
               </tr>
             </form>
